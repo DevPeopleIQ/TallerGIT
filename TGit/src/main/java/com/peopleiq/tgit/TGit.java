6e9017588ab58e56fe302afe6a5d5509d9992b68
DevPeopleIQ <peopleiq@outlook.com>
2025-05-18 20:08:20 -0600

    feat() Clase Main

---
+++ TGit/src/main/java/com/peopleiq/tgit/TGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.peopleiq.tgit;
+
+/**
+ *
+ * @author peopl
+ */
+public class TGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
