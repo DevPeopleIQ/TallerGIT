d0a0081b675b8d6a3b38448282fdbac6e9b43945
DevPeopleIQ <peopleiq@outlook.com>
2025-05-18 20:13:34 -0600

    feat() Clase Main

--- TGit/src/main/java/com/peopleiq/tgit/TGit.java
+++ TGit/src/main/java/com/peopleiq/tgit/TGit.java
@@ -11,6 +11,6 @@ package com.peopleiq.tgit;
 public class TGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!"); 
     }
 }
